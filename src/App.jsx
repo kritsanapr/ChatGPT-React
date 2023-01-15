685f7fcf70aff4672d58052ebfb2f6c9f86d35af
kritsanapr <kritsana.pr.41@gmail.com>
2023-01-15 23:04:33 +0700

    Complete mini AI app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react";
 import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
 import OptionSelection from "./components/OptionSelection";
 import Translation from "./components/Translation";
 import { arrayItems } from "./AIOptions";
+import { useState } from "react";
 
 function App() {
+  const configuration = new Configuration({
+    apiKey: import.meta.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({});
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+  // console.log(import.meta.env.VITE_Open_AI_Key);
   const selectOption = (option) => {
-    console.log(option);
+    setOption(option);
+  };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+    const response = await openai.createCompletion(object);
+    setResult(response.data.choices[0].text);
   };
 
   return (
-    <>
-      <h1 className="heading">AI APP</h1>
-      <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
-      <Translation />
-    </>
+    <div className="App">
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result} />
+      )}
+    </div>
   );
 }
 
